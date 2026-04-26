b96ad1c5f1950e451312293093db58595ec9e892
Fabian Enrique Suarez Carvajal <77165745+ing-fabiansuarez@users.noreply.github.com>
2026-04-25 20:09:39 -0500

    mejoras en registro: nombres reales y auto-login

--- users/forms.py
+++ users/forms.py
@@ -3,11 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class StudentSignupForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    first_name = forms.CharField(label="Nombre", max_length=30, required=True)
+    last_name = forms.CharField(label="Apellido", max_length=30, required=True)
+    email = forms.EmailField(label="Correo electrónico", required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "email")
+        fields = ("username", "first_name", "last_name", "email")
 
     def save(self, commit=True):
         user = super().save(commit=False)
--- users/views.py
+++ users/views.py
@@ -3,10 +3,17 @@ from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import StudentSignupForm
 
+from django.contrib.auth import login
+
 class StudentSignupView(CreateView):
     form_class = StudentSignupForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('index')
     template_name = 'users/signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
