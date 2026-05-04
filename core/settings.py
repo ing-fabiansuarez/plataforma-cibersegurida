c8b4487bf106557ac5b4e84482c214e28801a7fc
Fabian Enrique Suarez Carvajal <77165745+ing-fabiansuarez@users.noreply.github.com>
2026-05-04 09:58:01 -0500

    coanfigurado par aque se guarde media en otro servicio

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'main',
     'users',
     'announcements',
@@ -152,3 +154,12 @@ if not DEBUG:
 # Media files (uploaded images)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Cloudinary Configuration for Render
+if os.environ.get('RENDER'):
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
